4f624fc27209642ef4b4ba4778df0a53bc3591e4
TerraBaddie <45601356+TerraBaddie@users.noreply.github.com>
2026-08-28 01:33:26 -0400

    Add files via upload

---
+++ MainTank/Core/Release.lua
@@ -0,0 +1,7 @@
+-- MainTank release ownership
+-- Keep public version/build identification centralized here.
+if MainTank then
+    MainTank.packageVersion = "1.2.62"
+    MainTank.version = "1.2.62 FINALHOTFIX2"
+    MainTank.releaseBuild = "FINALHOTFIX2"
+end
