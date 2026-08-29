71d1b4f8958bf1b0f4d8fe92b1a221ef8f5420f6
TerraBaddie <45601356+TerraBaddie@users.noreply.github.com>
2026-08-29 17:55:09 -0400

    Update version to 1.2.63 with BOSSGUARD2 build

--- MainTank/Core/Release.lua
+++ MainTank/Core/Release.lua
@@ -1,7 +1,7 @@
 -- MainTank release ownership
 -- Keep public version/build identification centralized here.
 if MainTank then
-    MainTank.packageVersion = "1.2.62"
-    MainTank.version = "1.2.62 FINALHOTFIX2"
-    MainTank.releaseBuild = "FINALHOTFIX2"
+    MainTank.packageVersion = "1.2.63"
+    MainTank.version = "1.2.63 BOSSGUARD2"
+    MainTank.releaseBuild = "BOSSGUARD2"
 end
